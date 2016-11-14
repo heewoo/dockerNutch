0d04f94bbcde163064f1a1687b71e47e000343f4
heewoo <heewoo7139@gmail.com>
2016-11-14 20:48:28 +0900

    hbase-site.xml 수정

--- dockerNutch/deploy/nutch_hbase_conf.sh
+++ dockerNutch/deploy/nutch_hbase_conf.sh
@@ -7,16 +7,16 @@ echo "############################################"
 
 
 
-scp -r ./nutch-hbase-conf/hbase-site1.xml nutch1:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
-scp -r ./nutch-hbase-conf/hbase-site2.xml nutch2:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
-scp -r ./nutch-hbase-conf/hbase-site3.xml nutch3:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
-scp -r ./nutch-hbase-conf/hbase-site4.xml nutch4:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
-scp -r ./nutch-hbase-conf/hbase-site5.xml nutch5:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
-scp -r ./nutch-hbase-conf/hbase-site6.xml nutch6:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
-scp -r ./nutch-hbase-conf/hbase-site7.xml nutch7:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
-scp -r ./nutch-hbase-conf/hbase-site8.xml nutch8:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
-scp -r ./nutch-hbase-conf/hbase-site9.xml nutch9:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
-scp -r ./nutch-hbase-conf/hbase-site10.xml nutch10:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
+scp -r ./nutch-hbase-conf/hbase-site.xml nutch1:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
+scp -r ./nutch-hbase-conf/hbase-site.xml nutch2:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
+scp -r ./nutch-hbase-conf/hbase-site.xml nutch3:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
+scp -r ./nutch-hbase-conf/hbase-site.xml nutch4:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
+scp -r ./nutch-hbase-conf/hbase-site.xml nutch5:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
+scp -r ./nutch-hbase-conf/hbase-site.xml nutch6:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
+scp -r ./nutch-hbase-conf/hbase-site.xml nutch7:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
+scp -r ./nutch-hbase-conf/hbase-site.xml nutch8:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
+scp -r ./nutch-hbase-conf/hbase-site.xml nutch9:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
+scp -r ./nutch-hbase-conf/hbase-site.xml nutch10:/usr/lib/nutch/runtime/local/conf/hbase-site.xml 
 
 
 
