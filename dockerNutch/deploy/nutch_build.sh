27ce4574095cf4c692a2854858a71169fcf95d21
heewoo <heewoo7139@gmail.com>
2016-11-15 16:59:02 +0900

    nutch build 수정, url dir 스크립트 추가

--- dockerNutch/deploy/nutch_build.sh
+++ dockerNutch/deploy/nutch_build.sh
@@ -6,6 +6,17 @@ echo "NUTCH - BUILD START"
 echo "############################################"
 
 
+docker exec nutch1 "/usr/lib/deploy/nutch_build.sh" &
+docker exec nutch2 "/usr/lib/deploy/nutch_build.sh" &
+docker exec nutch3 "/usr/lib/deploy/nutch_build.sh" &
+docker exec nutch4 "/usr/lib/deploy/nutch_build.sh" &
+docker exec nutch5 "/usr/lib/deploy/nutch_build.sh" &
+docker exec nutch6 "/usr/lib/deploy/nutch_build.sh" &
+docker exec nutch7 "/usr/lib/deploy/nutch_build.sh" &
+docker exec nutch8 "/usr/lib/deploy/nutch_build.sh" &
+docker exec nutch9 "/usr/lib/deploy/nutch_build.sh" &
+docker exec nutch10 "/usr/lib/deploy/nutch_build.sh" &
+
  
 
 echo "############################################"
---
+++ dockerNutch/deploy/nutch_dir_url.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+echo "############################################"
+echo "NUTCH - DIR  START"
+echo "############################################"
+
+
+
+docker exec nutch1  mkdir -p /usr/lib/nutch/runtime/local/urls
+docker exec nutch2  mkdir -p /usr/lib/nutch/runtime/local/urls
+docker exec nutch3  mkdir -p /usr/lib/nutch/runtime/local/urls
+docker exec nutch4  mkdir -p /usr/lib/nutch/runtime/local/urls
+docker exec nutch5  mkdir -p /usr/lib/nutch/runtime/local/urls
+docker exec nutch6  mkdir -p /usr/lib/nutch/runtime/local/urls
+docker exec nutch7  mkdir -p /usr/lib/nutch/runtime/local/urls
+docker exec nutch8  mkdir -p /usr/lib/nutch/runtime/local/urls
+docker exec nutch9  mkdir -p /usr/lib/nutch/runtime/local/urls
+docker exec nutch10 mkdir -p /usr/lib/nutch/runtime/local/urls
+
+
+
+ 
+
+echo "############################################"
+echo "NUTCH - DIR  END"
+echo "############################################"
