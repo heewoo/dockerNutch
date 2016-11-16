3682f12a5a7a33feeeced50976c49391cdeef55a
heewoo <heewoo7139@gmail.com>
2016-11-16 21:35:48 +0900

    ssh init update

--- dockerFile/docker_ssh_init.sh
+++ dockerFile/docker_ssh_init.sh
@@ -12,8 +12,8 @@ touch /root/.ssh/known_hosts
 
 for ((i=1;i<11;i++));
         do
-                "ssh-keyscan nutch${i} 2>&1 | grep -v ^# >> /root/.ssh/known_hosts";
-		echo "nutch %{i} ssh init";
+                ssh-keyscan nutch${i} 2>&1 | grep -v ^# >> /root/.ssh/known_hosts;
+		echo "nutch" %{i}
         done
 
 
